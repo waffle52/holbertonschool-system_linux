ae0a7d2307c25ccfc9956f780dec1872144bf276
waffle52 <asuka9767@protonmail.com>
2021-03-05 21:05:50 -0800

    Uploading advanced

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+
 int handle_signal(void);
 void handle(int signum);
 void (*current_handler_signal(void))(int);
@@ -15,5 +16,10 @@ void empty_handle(int signum);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
 int pid_exist(pid_t pid);
+void all_in_one(void);
+int sigset_init(sigset_t *set, int *signals);
+int signals_block(int *signals);
+int signals_unblock(int *signals);
+int handle_pending(void (*handler)(int));
 
 #endif
