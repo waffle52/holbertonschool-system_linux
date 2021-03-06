431b68ea836d8eac4d4d92dec671e6eee4ada28b
waffle52 <asuka9767@protonmail.com>
2021-03-05 20:49:34 -0800

    Added comments to find error

--- 0x05-libasm/101-puti_base.asm
+++ 0x05-libasm/101-puti_base.asm
@@ -1,3 +1,4 @@
+
 BITS 64
 	global asm_puti_base
 	extern asm_putc
@@ -13,10 +14,12 @@ asm_puti_base:
 	push r12
 
 	xor r15, r15
-	xor r14, r14
-	movsx r15, edi
+	xor r14, r14	; r14 is our char counter
+	movsx r15, edi	; r15 holds our integer n
+	;mov rsi, r12
 	xor rax, rax
 
+	; print negative first then make positive
 	cmp r15, 0
 	jge after_negative
 	imul r15, -1
@@ -29,11 +32,13 @@ asm_puti_base:
 	pop rbx
 
 after_negative:
+	; get base string length;
 	mov rdi, rsi
 	call asm_strlen
-	mov rbx, rax
+	mov rbx, rax	; case 0?
 	cmp rbx, 1
 	jnz test_zero
+	; base is 1
 	mov r14, r15
 	loop_unary:
 	test r15, r15
@@ -48,6 +53,7 @@ after_negative:
 	jmp while_n
 
 test_zero:
+	; handle case where n == 0
 	test r15, r15
 	jnz while_n
 	movzx rdi, BYTE [rsi]
@@ -57,10 +63,12 @@ test_zero:
 while_n:
 	test rbx, rbx
 	jz while_n_nobase
+	; RDX receives remainder
 	xor rdx, rdx
 	test r15, r15
 	jz done_while_n
 	mov rax, r15
+	;mov rbx, 10
 	div rbx
 	mov r15, rax
 	xor rax, rax
@@ -72,12 +80,15 @@ done_while_n:
 	jmp done_both_while
 
 while_n_nobase:
+	; RAX starts as dividend becomes quotient
+	; RDX receives remainder
 	xor rdx, rdx
 	test r15, r15
 	jz done_while_n_nobase
 	mov rax, r15
 	mov rbx, 10
 	div rbx
+	; print remainder in RDX
 	mov r15, rax
 	xor rax, rax
 	mov rax, rdx
@@ -88,12 +99,13 @@ while_n_nobase:
 done_while_n_nobase:
 
 done_both_while:
-	mov r15, r14
+	mov r15, r14;	move counter
 print_loop:
 	test r15, r15
 	jz done_print_loop
 	pop rdi
 	after_pop:
+	;add rdi, 48
 	call asm_putc
 	dec r15
 	jmp print_loop
