785e6eae83746aaa0a8a188a2fab76cb2b957d90
waffle52 <asuka9767@protonmail.com>
2021-03-07 12:58:17 -0800

    Added library

--- 0x05-libasm/libasm.h
+++ 0x05-libasm/libasm.h
@@ -1,6 +1,8 @@
 #ifndef _LIBASM_H_
 #define _LIBASM_H_
 
+#include <stdio.h>
+
 size_t asm_strlen(const char *str);
 int asm_strcmp(const char *s1, const char *s2);
 int asm_strncmp(const char *s1, const char *s2, size_t n);
